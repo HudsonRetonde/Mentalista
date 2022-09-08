e2e2ca0bb3b2ade9d1cd20654a423712ff1bc8da
Hudson Retonde <hudsonsretonde@gmail.com>
2022-09-08 16:30:23 -0300

    Projeto finalizado, condição de número máximo de três tentativas.

--- access/main.js
+++ access/main.js
@@ -1,21 +1,7 @@
-/*var numeroSecreto = parseInt(Math.random() *11);
-
-function Chutar() {
-    var elementoResultado = document.getElementById("resultado");
-    var chute = parseInt(document.getElementById("valor").value);
-    console.log(chute);
-    if(chute == numeroSecreto) {
-        elementoResultado.innerHTML = "Parabéns, você acertou!!!" 
-    } else if(chute > 10 || chute < 0){
-        elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10..." 
-    } else {
-        elementoResultado.innerHTML = "Eh... você errou!!!"
-    }
-}*/
-
 var numeroSecreto = parseInt(Math.random() *11);
 
-var tentativa = 1;
+
+var tentativas = 1;
 const totalTentativas = 3;
  
 function Chutar() {
@@ -27,15 +13,19 @@ function Chutar() {
         elementoResultado.innerHTML = "Parabéns, você acertou!!!" 
     } else if(chute > 10 || chute < 0){
         elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10..." 
+    } else if ( tentativas >= totalTentativas) {
+        elementoResultado.innerHTML = "Game Over! Seu número de tentativas acabou, o número secreto era " + numeroSecreto + "."; 
     } else {
         
         if(numeroSecreto > chute) {
             elementoResultado.innerHTML = "Você errou! O Número secreto é maior que o número digitado. Tente novamente!"
-        } else {
+            tentativas++;
+            console.log(tentativas)
+        } else if (numeroSecreto < chute) {
             elementoResultado.innerHTML = "Você errou! O Número secreto é menor que o número digitado. Tente novamente!"
-        }
+            tentativas++
+            console.log(tentativas)
+        } 
     }
-   // tentativa ++
-    //console.log(tentativa);
-    //
+   
 }
